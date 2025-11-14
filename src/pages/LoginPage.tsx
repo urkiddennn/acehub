04c0e6add3c942691a77d245afd9bd58cef6a250
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-11-14 21:45:52 +0800

    Update: created the basics

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,7 +15,8 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="w-96 p-8 bg-white shadow-xl rounded-lg text-center">
+    <div className="w-96 p-8 bg-white shadow-xl rounded-lg text-center flex flex-col gap-3">
+      <h1 className="text-2xl font-bold">Login</h1>
       <label className="input validator">
         <svg
           className="h-[1em] opacity-50"
@@ -47,6 +48,52 @@ const LoginPage: React.FC = () => {
         Must be 3 to 30 characters
         <br />
         containing only letters, numbers or dash
+      </p>
+
+      <label className="input validator">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+            <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+          </g>
+        </svg>
+        <input
+          type="password"
+          required
+          placeholder="Password"
+          minlength="8"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+        />
+      </label>
+      <p className="validator-hint hidden">
+        Must be more than 8 characters, including
+        <br />
+        At least one number <br />
+        At least one lowercase letter <br />
+        At least one uppercase letter
+      </p>
+      <button
+        className="btn w-full bg-gray-900 text-white"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
+      <p>
+        Don't have an account?{" "}
+        <a href="" className="text-blue-500 cursor-pointer">
+          Request
+        </a>
       </p>
     </div>
   );
